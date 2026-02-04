700729317a8590c17dc551481a398a59f8294cd9
Ajinkyawagh4846 <ajinkyawagh2005@gmail.com>
2026-02-04 10:22:18 +0530

    Fix Vercel static asset deployment

--- frontend/config.js
+++ frontend/config.js
@@ -1,6 +1,5 @@
-// Shared configuration
-// Check if in production
+
 const API_URL = window.location.hostname === 'localhost' 
-    ? 'http://127.0.0.1:5000' 
-    : 'https://movie-reviewer-9nhp.onrender.com';  // Your Render URL
+    ? 'https://movie-reviewer-9nhp.onrender.com' 
+    : 'http://127.0.0.1:5000';  // Your Render URL
 // const API_URL = 'http://127.0.0.1:5000';
