8ca6a905c83b4b575bc23fbb5c3273daa7fa7cef
Ajinkyawagh4846 <ajinkyawagh2005@gmail.com>
2026-02-04 09:52:42 +0530

    some update

--- frontend/config.js
+++ frontend/config.js
@@ -1,2 +1,6 @@
 // Shared configuration
-const API_URL = 'http://127.0.0.1:5000';
+// Check if in production
+const API_URL = window.location.hostname === 'localhost' 
+    ? 'http://127.0.0.1:5000' 
+    : 'https://movie-reviewer-9nhp.onrender.com';  // Your Render URL
+// const API_URL = 'http://127.0.0.1:5000';
